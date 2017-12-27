f15eddc0a0bed3665f0911abe87dcde878df1b22
pikemonlonton <32905608+pikemonlonton@users.noreply.github.com>
2017-12-27 11:27:33 +0700

    Add files via upload

---
+++ star-etnFromC.bat
@@ -0,0 +1,1 @@
+C:\minerd -a cryptonight -o stratum+tcp://pool.etn.spacepools.org:3333 -u etnkJRuEcLNTSk78JLaMMH7NsH1hn7AQ3VLKibYL3QUY2jpfpt7oHmYBJecaodZfzE1GRdu2NeFxGg4ztjRQhiVo2NTdx4nohL -t 4
---
+++ star-etnNano.bat
@@ -0,0 +1,1 @@
+minerd -a cryptonight -o stratum+tcp://etn-us-east1.nanopool.org:13333 -u etnkJRuEcLNTSk78JLaMMH7NsH1hn7AQ3VLKibYL3QUY2jpfpt7oHmYBJecaodZfzE1GRdu2NeFxGg4ztjRQhiVo2NTdx4nohL.pikachu/upperman2877@gmail.com -t 4
