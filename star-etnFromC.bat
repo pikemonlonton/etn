ac90d18e9db5e6ed1d27e96056af9428778257cc
pikemonlonton <32905608+pikemonlonton@users.noreply.github.com>
2018-01-04 18:47:48 +0700

    Update star-etnFromC.bat

--- star-etnFromC.bat
+++ star-etnFromC.bat
@@ -1,1 +1,1 @@
-C:\minerd -a cryptonight -o stratum+tcp://pool.etn.spacepools.org:3333 -u etnkJRuEcLNTSk78JLaMMH7NsH1hn7AQ3VLKibYL3QUY2jpfpt7oHmYBJecaodZfzE1GRdu2NeFxGg4ztjRQhiVo2NTdx4nohL -t 4
+C:\minerd -a cryptonight -o stratum+tcp://etn-us-east1.nanopool.org:13333 -u etnkJRuEcLNTSk78JLaMMH7NsH1hn7AQ3VLKibYL3QUY2jpfpt7oHmYBJecaodZfzE1GRdu2NeFxGg4ztjRQhiVo2NTdx4nohL -t 4
